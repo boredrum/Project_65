f5c790ee7730106d49c0aecf9fcc09de142a9ffc
boredrum <vvmaiboroda@gmail.com>
2023-08-01 18:03:34 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,63 @@
+import express from "express";
+import mongoose from "mongoose";
+import { User } from "./model/user.js";
+
+const url = "mongodb://localhost:27017/users";
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static("style"));
+
+app.use(express.urlencoded({extended: true}))
+
+app.set("view engine", "pug");
+
+mongoose
+	.connect(url)
+	.then(() => {
+		console.log("Connected to DB");
+		app.listen(PORT, () => {
+			console.log(`Server started on http://localhost:${PORT}`);
+		});
+	})
+	.catch((err) => {
+		console.log(`DB connection error: ${err}`);
+	});
+
+app.get("/", async (req, res) => {
+	try {
+		const users = await User.find();
+		res.render("index", { users });
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+app.get("/edit/:id", async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		res.render("userEdit", { user });
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+app.post("/change-user/:id", async (req, res) => {
+	try {
+		await User.findByIdAndUpdate(req.params.id, req.body);
+		res.redirect("/");
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+app.delete("/remove/:id", async (req, res) => {
+	try {
+		await User.deleteOne({ _id: req.params.id });
+		res.status(200).json({ message: "User deleted" });
+	} catch (err) {
+		console.log(err);
+	}
+});
